0e3d20cc760f084f1ffeedcb5d64c1821677e806
Keith Winkler <kwinkler@splicemachine.com>
2015-11-03 09:45:21 -0600

    scala

---
+++ src/com/redshiftsoft/example/scala/HelloWorld.scala
@@ -0,0 +1,12 @@
+package com.redshiftsoft.example.scala
+
+/**
+  * Created by keith on 11/3/15.
+  */
+class HelloWorld {
+
+  def main(args: Array[String]) {
+    println("Hello, world!")
+  }
+
+}
