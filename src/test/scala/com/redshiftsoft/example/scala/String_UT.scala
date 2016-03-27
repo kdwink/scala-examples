20774ec68199f2b51617a31f31884e3484d043aa
Keith David Winkler <keithdwinkler@gmail.com>
2016-03-27 14:07:01 -0500

    first test

---
+++ src/test/scala/com/redshiftsoft/example/scala/String_UT.scala
@@ -0,0 +1,19 @@
+package com.redshiftsoft.example.scala
+
+import org.junit.Test
+
+class String_UT {
+
+
+  @Test def verifyEasy() {
+    val multiLineString =
+      """
+        stuff
+        and more stuff
+      """.stripMargin
+
+
+  }
+
+
+}
