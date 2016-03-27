f3665b47f1935bdc0c2a44c1e92c5af5e00eb6a3
Keith David Winkler <keithdwinkler@gmail.com>
2016-03-27 14:19:29 -0500

    first test

--- src/test/scala/com/redshiftsoft/example/scala/Values_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Values_UT.scala
@@ -7,10 +7,17 @@ class Values_UT {
   @Test
   def declaring(): Unit = {
 
-    val x = 20
-    val y: Int = 20
+    val x1 = 20
+    val y1 = "abcd"
+    val z1 = 'c'
 
-    Assert.assertEquals(x, y)
+    val x2: Int = 20
+    val y2: String = "abcd"
+    val z2: Char = 'c'
+
+    Assert.assertEquals(x1, x2)
+    Assert.assertEquals(y1, y2)
+    Assert.assertEquals(z1, z2)
   }
 
 
