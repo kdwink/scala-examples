183208a925aea602baffcb22a692b7927a925d51
Keith David Winkler <keithdwinkler@gmail.com>
2016-03-27 19:32:49 -0500

    setup as maven project

--- src/test/scala/com/redshiftsoft/example/scala/Expressions_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Expressions_UT.scala
@@ -24,5 +24,19 @@ class Expressions_UT {
     Assert.assertEquals(106, x)
   }
 
+  @Test
+  def expressionIfElse(): Unit = {
+    val x = 10
+    val y = 100
+    val z = if (x > y) -10 else 10
+    Assert.assertEquals(10, z)
+
+    var flag = false
+    if (x > 1) {
+      flag = true
+    }
+    Assert.assertTrue(flag)
+  }
+
 
 }
