596ba8a3a1900add8365ad77b6cb18f2e13750c6
keith <keithdwinkler@gmail.com>
2016-03-28 13:51:52 -0500

    tests

--- src/test/scala/com/redshiftsoft/example/scala/Tuple_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Tuple_UT.scala
@@ -5,10 +5,10 @@ import org.junit.{Assert, Test}
 class Tuple_UT {
 
   @Test def tuple(): Unit = {
-    val x = ("keith", 2, 3.14159);
-    Assert.assertEquals("keith", x._1);
-    Assert.assertEquals(2, x._2);
-    Assert.assertEquals(3.14159d, x._3, 1e-9);
+    val x = ("keith", 2, 3.14159)
+    Assert.assertEquals("keith", x._1)
+    Assert.assertEquals(2, x._2)
+    Assert.assertEquals(3.14159d, x._3, 1e-9)
   }
 
 }
