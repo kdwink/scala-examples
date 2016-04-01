32fc1c7f80eaa433b4e8f884a8f8bfc1d52f3941
keith <keithdwinkler@gmail.com>
2016-04-01 16:20:45 -0500

    tests

--- src/test/scala/com/redshiftsoft/example/scala/Class_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Class_UT.scala
@@ -43,4 +43,23 @@ class Class_UT {
     Assert.assertEquals("hello B", c.method2)
   }
 
+  @Test def polymorphism(): Unit = {
+    class A {
+      def method1 = "hello 1"
+
+      def method2 = "hello 2"
+    }
+    class B extends A {
+      override def method2 = "hello B"
+    }
+    class C extends B
+
+    val c1: A = new C
+    val c2: B = new C
+    val c3: C = new C
+    Assert.assertEquals("hello B", c1.method2)
+    Assert.assertEquals("hello B", c1.method2)
+    Assert.assertEquals("hello B", c1.method2)
+  }
+
 }
