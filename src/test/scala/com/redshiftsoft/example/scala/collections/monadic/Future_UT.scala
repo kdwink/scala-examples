cb8d53ab7aa146ec9c853a7672316cf79bb94c9a
Keith David Winkler <keithdwinkler@gmail.com>
2016-04-06 19:00:57 -0500

    first tests

--- src/test/scala/com/redshiftsoft/example/scala/collections/monadic/Future_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/monadic/Future_UT.scala
@@ -48,11 +48,11 @@ class Future_UT {
       List(
         Future(sleepFunction(12000)), // really slow
         Future(sleepFunction(120)),
-        Future(sleepFunction(60)),
+        Future(sleepFunction(90)),
         Future(sleepFunction(-1)), // throws exception
         Future(sleepFunction(240)),
         Future(sleepFunction(3)),
-        Future(sleepFunction(90)),
+        Future(sleepFunction(99)),
         Future(sleepFunction(6000)) // pretty slow
       )
     )
