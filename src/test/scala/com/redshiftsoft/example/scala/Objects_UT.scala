0c1365ddee8f0045d7c84e18ddd413303a50c1be
keith <keithdwinkler@gmail.com>
2016-04-06 22:22:19 -0500

    tests

--- src/test/scala/com/redshiftsoft/example/scala/Objects_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Objects_UT.scala
@@ -1,11 +1,11 @@
 package com.redshiftsoft.example.scala
 
-import org.junit.Test
+import org.junit.{Assert, Test}
 
 class Objects_UT {
 
   @Test
-  def go(): Unit = {
+  def basicUsage(): Unit = {
 
     object MyObject {
       val WHATEVER = 100
@@ -15,6 +15,24 @@ class Objects_UT {
 
     MyObject.age = 20
     MyObject.name = "keith"
+
+  }
+
+  @Test
+  def apply(): Unit = {
+
+    object MyObject {
+      val WHATEVER = 100
+      var age: Int = 10
+      var name: String = "george"
+
+      def apply(s: String): Unit = {
+        this.name = s
+      }
+    }
+
+    MyObject("Stuff")
+    Assert.assertEquals("Stuff", MyObject.name)
 
   }
 
