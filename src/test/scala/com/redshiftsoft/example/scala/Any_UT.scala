9f2197dfd65f03ddb575fcb16927c28efae3478d
keith <keithdwinkler@gmail.com>
2016-04-07 17:08:37 -0500

    tests

---
+++ src/test/scala/com/redshiftsoft/example/scala/Any_UT.scala
@@ -0,0 +1,28 @@
+package com.redshiftsoft.example.scala
+
+import org.junit.{Assert, Test}
+
+class Any_UT {
+
+  class Person(n: String, a: Int) {
+    val name: String = n
+    val age: Int = a
+
+    override def equals(other: Any): Boolean = {
+      if (!other.isInstanceOf[Person]) return false
+      val otherPerson = other.asInstanceOf[Person]
+      this.name.equals(otherPerson.name) && Math.abs(age - otherPerson.age) < 10
+    }
+  }
+
+  @Test def equals(): Unit = {
+    val person1 = new Person("keith", 200)
+    val person2 = new Person("keith", 205)
+    val person3 = new Person("keith", 300)
+
+    Assert.assertTrue(person1 == person2)
+    Assert.assertFalse(person1 == person3)
+
+  }
+
+}
