b406e94ebba9e491ae67b30a4dcd902d7dfd72eb
keith <keithdwinkler@gmail.com>
2016-04-10 22:19:32 -0500

    tests

--- src/test/scala/com/redshiftsoft/example/scala/Tuple_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Tuple_UT.scala
@@ -4,15 +4,21 @@ import org.junit.{Assert, Test}
 
 class Tuple_UT {
 
-  @Test def tuple(): Unit = {
-    val x = ("keith", 2, 3.14159)
-    Assert.assertEquals("keith", x._1)
-    Assert.assertEquals(2, x._2)
-    Assert.assertEquals(3.14159d, x._3, 1e-9)
+  @Test def declaring(): Unit = {
+    val t1 = ("keith", 100, 3.14159)
+    val t2: (String, Int, Double) = ("keith", 100, 3.14159)
+
+
+    for (x <- List(t1, t2)) {
+      Assert.assertEquals("keith", x._1)
+      Assert.assertEquals(100, x._2)
+      Assert.assertEquals(3.14159d, x._3, 1e-9)
+    }
   }
 
-  @Test def tuple2(): Unit = {
+  @Test def declarigOperator(): Unit = {
     val x = "keith" -> 2
+
     Assert.assertEquals("keith", x._1)
     Assert.assertEquals(2, x._2)
   }
