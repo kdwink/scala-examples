c625696800b517ee1881fd32d06821abb348f2cf
keith <keithdwinkler@gmail.com>
2016-04-10 22:30:10 -0500

    tests

--- src/test/scala/com/redshiftsoft/example/scala/Tuple_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Tuple_UT.scala
@@ -7,9 +7,10 @@ class Tuple_UT {
   @Test def declaring(): Unit = {
     val t1 = ("keith", 100, 3.14159)
     val t2: (String, Int, Double) = ("keith", 100, 3.14159)
+    val t3: (String, Int, Double) = new Tuple3[String, Int, Double]("keith", 100, 3.14159)
 
 
-    for (x <- List(t1, t2)) {
+    for (x <- List(t1, t2, t3)) {
       Assert.assertEquals("keith", x._1)
       Assert.assertEquals(100, x._2)
       Assert.assertEquals(3.14159d, x._3, 1e-9)
