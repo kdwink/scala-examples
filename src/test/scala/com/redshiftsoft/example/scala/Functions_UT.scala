95cabd9521a36fe3791b5f9c7c56ca958c50241c
keith <keithdwinkler@gmail.com>
2016-04-10 23:06:24 -0500

    tests

--- src/test/scala/com/redshiftsoft/example/scala/Functions_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Functions_UT.scala
@@ -115,4 +115,16 @@ class Functions_UT {
     Assert.assertEquals(60, triple(accessDBFunc))
   }
 
+  @Test def andThen(): Unit = {
+    val doubler = (i: Int) => i * 2
+    val plus3 = (i: Int) => i + 3
+    Assert.assertEquals(203, (doubler andThen plus3) (100))
+  }
+
+  @Test def compose(): Unit = {
+    val doubler = (i: Int) => i * 2
+    val plus3 = (i: Int) => i + 3
+    Assert.assertEquals(206, (doubler compose plus3) (100))
+  }
+
 }
