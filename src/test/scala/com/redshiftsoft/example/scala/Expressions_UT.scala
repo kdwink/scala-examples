ac99266c9ff7761f166efd706d48b725f6c81692
keith <keithdwinkler@gmail.com>
2016-04-15 16:29:13 -0500

    tests

--- src/test/scala/com/redshiftsoft/example/scala/Expressions_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Expressions_UT.scala
@@ -37,4 +37,18 @@ class Expressions_UT {
     Assert.assertTrue(flag)
   }
 
+  @Test def ifExpression() {
+    val string1: String = null
+    val string2: String = "hello"
+
+    val r1 = if (string1 == null) "x" else string1
+    val r2 = if (string2 == null) "y" else string2
+
+    Assert.assertTrue(r1.isInstanceOf[String])
+    Assert.assertTrue(r2.isInstanceOf[String])
+
+    Assert.assertEquals("x", r1)
+    Assert.assertEquals("hello", r2)
+  }
+
 }
