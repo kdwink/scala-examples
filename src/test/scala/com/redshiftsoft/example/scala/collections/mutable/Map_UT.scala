0b29138ad4b865ac5d4a9175bbe99c138f2099af
keith <keithdwinkler@gmail.com>
2016-04-18 14:08:33 -0500

    tests

---
+++ src/test/scala/com/redshiftsoft/example/scala/collections/mutable/Map_UT.scala
@@ -0,0 +1,15 @@
+package com.redshiftsoft.example.scala.collections.mutable
+
+import org.junit.{Assert, Test}
+
+import scala.collection.mutable
+
+class Map_UT {
+
+  @Test def put(): Unit = {
+    val map: mutable.Map[String, String] = mutable.HashMap.empty
+    map.put("key1", "value1")
+    Assert.assertEquals(1, map.size)
+  }
+
+}
