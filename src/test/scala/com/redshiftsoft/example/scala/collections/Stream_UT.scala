b3221a5a70307384e836e4ba1332ca08afd8eef9
keith <keithdwinkler@gmail.com>
2016-04-20 13:14:31 -0500

    tests

--- src/test/scala/com/redshiftsoft/example/scala/collections/Stream_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/Stream_UT.scala
@@ -27,4 +27,15 @@ class Stream_UT {
     Assert.assertEquals(List(1, 2, 3, 4, 5), streamResult2.take(5).toList)
   }
 
+
+  @Test def find(): Unit = {
+    def fibFrom(a: Int, b: Int): Stream[Int] = a #:: fibFrom(b, a + b)
+
+    val streamResult = fibFrom(1, 1)
+    Assert.assertEquals(List(1, 1, 2, 3, 5, 8, 13), streamResult.take(7).toList)
+
+    val found: Option[Int] = streamResult.find(_ > 10)
+    Assert.assertEquals(13, found.get)
+  }
+
 }
