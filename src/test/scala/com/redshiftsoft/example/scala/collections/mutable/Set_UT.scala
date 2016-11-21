3b6b323e20f6182c6b93fb97da8c82595c1e9c5c
keith <keithdwinkler@gmail.com>
2016-11-21 11:43:25 -0600

    Set_UT

---
+++ src/test/scala/com/redshiftsoft/example/scala/collections/mutable/Set_UT.scala
@@ -0,0 +1,16 @@
+package com.redshiftsoft.example.scala.collections.mutable
+
+import org.junit.Test
+
+import scala.collection.mutable
+
+class Set_UT {
+
+  @Test
+  def test(): Unit = {
+
+    val fruit = mutable.Set("apple", "orange", "peach", "banana")
+
+  }
+
+}
