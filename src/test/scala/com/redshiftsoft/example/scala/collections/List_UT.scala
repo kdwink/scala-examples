223e0fa5466428dcd2f5855457329e4e42018d4a
keith <keithdwinkler@gmail.com>
2016-12-06 08:28:06 -0600

    List_UT

--- src/test/scala/com/redshiftsoft/example/scala/collections/List_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/List_UT.scala
@@ -253,5 +253,13 @@ class List_UT {
     assertEquals(110, result)
   }
 
+  @Test def groupBy(): Unit = {
+    val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val resultMap = numbers.groupBy(f => f % 2)
+
+    assertEquals(2, resultMap.size)
+    assertEquals("List(2, 4, 6, 8)", resultMap(0).toString)
+    assertEquals("List(1, 3, 5, 7, 9)", resultMap(1).toString)
+  }
 
 }
