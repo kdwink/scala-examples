2605cfed29326379cb3cb20d87ba4e7cdbc29341
keith <keithdwinkler@gmail.com>
2017-03-01 14:40:58 -0600

    range

---
+++ src/test/scala/com/redshiftsoft/example/scala/Range_UT.scala
@@ -0,0 +1,14 @@
+package com.redshiftsoft.example.scala
+
+import org.junit.Test
+
+class Range_UT {
+
+  @Test
+  def literals(): Unit = {
+    val range = 1 to 10
+    val range2 = 1.0 to 15.5 by .5
+
+  }
+
+}
