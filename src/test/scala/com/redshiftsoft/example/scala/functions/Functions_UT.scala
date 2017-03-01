08224af951b6b3611bc487685fba446a7046400e
keith <keithdwinkler@gmail.com>
2017-03-01 13:13:48 -0600

    functions

--- src/test/scala/com/redshiftsoft/example/scala/functions/Functions_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/functions/Functions_UT.scala
@@ -4,21 +4,7 @@ import org.junit.{Assert, Test}
 
 class Functions_UT {
 
-  @Test def tailRecursiveDoNotStackOverflow(): Unit = {
-    @annotation.tailrec
-    def add(n: Int, sum: Int): Int = {
-      if (n < 1) {
-        return sum
-      }
-      add(n - 1, sum + n)
-    }
-    Assert.assertEquals(10, add(4, 0))
-    Assert.assertEquals(80200, add(400, 0))
-    Assert.assertEquals(800020000, add(40000, 0))
-  }
-
   @Test def namedParameters(): Unit = {
-    @annotation.tailrec
     def add(n: Int, sum: Int): Int = {
       if (n < 1) {
         return sum
