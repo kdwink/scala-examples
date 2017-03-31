ce4bff7802ebfaf1d16a9356c415325592bfaa85
keith <keithdwinkler@gmail.com>
2017-03-31 10:41:53 -0500

    Operator_UT

--- src/test/scala/com/redshiftsoft/example/scala/Operators_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Operators_UT.scala
@@ -12,10 +12,28 @@ class Operators_UT {
     Assert.assertEquals(142, answer2)
   }
 
-  @Test
-  def mod(): Unit = {
+  @Test def mod(): Unit = {
     val x = 20 % 7
     Assert.assertEquals(6, x)
+  }
+
+  @Test def overloading(): Unit = {
+
+    class Ball(initialSum: Int) {
+      def +(ball: Ball): Unit = {
+        this.sum = this.sum + ball.sum
+      }
+
+      var sum: Int = initialSum
+    }
+
+    val b1 = new Ball(10)
+    val b2 = new Ball(20)
+
+    b1 + b2
+
+    Assert.assertEquals(30, b1.sum)
+
   }
 
 }
