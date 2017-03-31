f19658476cb26b9e60b375b7af7c3f33e23f6cec
keith <keithdwinkler@gmail.com>
2017-03-31 10:36:26 -0500

    Operator_UT

--- src/test/scala/com/redshiftsoft/example/scala/Operators_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Operators_UT.scala
@@ -4,6 +4,14 @@ import org.junit.{Assert, Test}
 
 class Operators_UT {
 
+  @Test def operatorsAreMethods(): Unit = {
+    val answer1 = 100 + 42
+    val answer2 = 100.+(42)
+
+    Assert.assertEquals(142, answer1)
+    Assert.assertEquals(142, answer2)
+  }
+
   @Test
   def mod(): Unit = {
     val x = 20 % 7
