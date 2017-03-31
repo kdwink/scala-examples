637c69c99f628c75ffe582b600eff6859d2cd45f
keith <keithdwinkler@gmail.com>
2017-03-31 15:28:17 -0500

    improve

--- src/test/scala/com/redshiftsoft/example/scala/functions/Functions_TailRecursive_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/functions/Functions_TailRecursive_UT.scala
@@ -5,17 +5,19 @@ import org.junit.{Assert, Test}
 class Functions_TailRecursive_UT {
 
   @Test def tailRecursiveDoNotStackOverflow(): Unit = {
+
     @annotation.tailrec
-    def add(n: Int, sum: Int): Int = {
-      if (n < 1) {
+    def add(n: Long, sum: Long): Long = {
+      if (n < 1L) {
         return sum
       }
-      add(n - 1, sum + n)
+      add(n - 1L, sum + n)
     }
 
-    Assert.assertEquals(10, add(4, 0))
-    Assert.assertEquals(80200, add(400, 0))
-    Assert.assertEquals(800020000, add(40000, 0))
+    Assert.assertEquals(10L, add(4, 0))
+    Assert.assertEquals(80200L, add(400, 0))
+    Assert.assertEquals(800020000L, add(40 * 1000, 0))
+    Assert.assertEquals(8000002000000L, add(4 * 1000 * 1000, 0))
   }
 
 }
