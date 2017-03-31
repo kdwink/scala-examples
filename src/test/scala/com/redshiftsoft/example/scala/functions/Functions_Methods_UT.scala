840695d02e4322d836a49906c3f3fe48b3a7ebee
keith <keithdwinkler@gmail.com>
2017-03-31 15:48:40 -0500

    improve

--- src/test/scala/com/redshiftsoft/example/scala/functions/Functions_Methods_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/functions/Functions_Methods_UT.scala
@@ -2,6 +2,9 @@ package com.redshiftsoft.example.scala.functions
 
 import org.junit.{Assert, Test}
 
+/**
+  * Tests of the functions in/on scala.Function
+  */
 class Functions_Methods_UT {
 
   @Test def andThen(): Unit = {
@@ -14,6 +17,14 @@ class Functions_Methods_UT {
     val doubler = (i: Int) => i * 2
     val plus3 = (i: Int) => i + 3
     Assert.assertEquals(206, (doubler compose plus3) (100))
+  }
+
+  @Test def tupled(): Unit = {
+    val multiply = (d1: Long, d2: Long) => d1 * d2
+    val multiplyTuple = Function.tupled(multiply)
+
+    Assert.assertEquals(12, multiply(3, 4))
+    Assert.assertEquals(30, multiplyTuple((5, 6)))
   }
 
 }
