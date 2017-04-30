35c718c203f47974ea494a8a9383661c1cfec576
keith <keithdwinkler@gmail.com>
2017-04-30 14:46:04 -0500

    improve

--- src/test/scala/com/redshiftsoft/example/scala/collections/Vector_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/Vector_UT.scala
@@ -18,4 +18,14 @@ class Vector_UT {
     Assert.assertEquals("Vector(1, 2, 3, 10, 20, 30)", v3.toString())
   }
 
+  @Test
+  def toMap(): Unit = {
+    val v = Vector(1, 2, 3, 4, 5)
+
+    val map = v.map(x => (x, 20 + x)).toMap
+
+    Assert.assertEquals(24, map(4))
+    Assert.assertEquals(25, map(5))
+  }
+
 }
