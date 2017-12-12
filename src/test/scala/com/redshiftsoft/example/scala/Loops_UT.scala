02f0d4fec40bc08feba42fbf7b7625117fa0f669
Keith David Winkler <kdwink@monsanto.com>
2017-12-12 09:11:07 -0600

    map

--- src/test/scala/com/redshiftsoft/example/scala/Loops_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Loops_UT.scala
@@ -6,7 +6,7 @@ import scala.collection.immutable.Seq
 
 class Loops_UT {
 
-  @Test def forYields() = {
+  @Test def forYields(): Unit = {
     val result: Seq[String] = for {x <- 1 to 3
                                    y <- 4 to 5} yield {
       s"$x-$y"
@@ -14,7 +14,7 @@ class Loops_UT {
     Assert.assertEquals("Vector(1-4, 1-5, 2-4, 2-5, 3-4, 3-5)", result.toString)
   }
 
-  @Test def forNested() = {
+  @Test def forNested(): Unit = {
     val twos = for (x <- 1 to 7) yield {
       2 * x
     }
@@ -23,6 +23,14 @@ class Loops_UT {
     }
     Assert.assertEquals("Vector(2, 4, 6, 8, 10, 12, 14)", twos.toString)
     Assert.assertEquals("Vector(3, 6, 9, 12, 15, 18)", threes.toString)
+  }
+
+  @Test def forOneToOne(): Unit = {
+    var loop = false
+    for(_ <- 1 to 1) {
+      loop = true
+    }
+    Assert.assertTrue(loop)
   }
 
 }
