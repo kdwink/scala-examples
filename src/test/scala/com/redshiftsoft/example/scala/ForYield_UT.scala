5cb643bbab66142614a9ae17a933307f3f23cc79
Keith David Winkler <kdwink@monsanto.com>
2018-09-11 09:25:19 -0500

    ForYield_UT

--- src/test/scala/com/redshiftsoft/example/scala/ForYield_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/ForYield_UT.scala
@@ -1,8 +1,9 @@
 package com.redshiftsoft.example.scala
 
-import org.junit.{Assert, Test}
+import org.junit.Test
 
 import scala.collection.immutable.Seq
+import org.junit.Assert.assertEquals
 
 class ForYield_UT {
 
@@ -10,19 +11,19 @@ class ForYield_UT {
     val twos = for (x <- 1 to 7) yield {
       2 * x
     }
-    Assert.assertEquals("Vector(2, 4, 6, 8, 10, 12, 14)", twos.toString)
+    assertEquals("Vector(2, 4, 6, 8, 10, 12, 14)", twos.toString)
   }
 
   @Test def for_yield_alternate_syntax(): Unit = {
     val twos = for {x <- 1 to 7} yield 2 * x
-    Assert.assertEquals("Vector(2, 4, 6, 8, 10, 12, 14)", twos.toString)
+    assertEquals("Vector(2, 4, 6, 8, 10, 12, 14)", twos.toString)
   }
 
   @Test def for_yield_with_guard(): Unit = {
     val threes = for (x <- 1 to 20 if x % 3 == 0) yield {
       x
     }
-    Assert.assertEquals("Vector(3, 6, 9, 12, 15, 18)", threes.toString)
+    assertEquals("Vector(3, 6, 9, 12, 15, 18)", threes.toString)
   }
 
   @Test def for_yield_two_variables(): Unit = {
@@ -30,7 +31,7 @@ class ForYield_UT {
                                    y <- 4 to 5} yield {
       s"$x-$y"
     }
-    Assert.assertEquals("Vector(1-4, 1-5, 2-4, 2-5, 3-4, 3-5)", result.toString)
+    assertEquals("Vector(1-4, 1-5, 2-4, 2-5, 3-4, 3-5)", result.toString)
   }
 
   @Test def for_yield_two_variables_second_dependent_on_first(): Unit = {
@@ -38,7 +39,7 @@ class ForYield_UT {
                                    y = 2 * x} yield {
       s"$x-$y"
     }
-    Assert.assertEquals("Vector(1-2, 2-4, 3-6, 4-8, 5-10, 6-12)", result.toString)
+    assertEquals("Vector(1-2, 2-4, 3-6, 4-8, 5-10, 6-12)", result.toString)
   }
 
 
