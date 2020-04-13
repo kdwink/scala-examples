603666bc4e72bc393b794e66aaca555ab6678a1f
Keith David Winkler <kdwink@monsanto.com>
2020-04-13 14:18:00 -0500

    improve

--- src/test/scala/com/redshiftsoft/example/scala/collections/mutable/Set_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/mutable/Set_UT.scala
@@ -7,14 +7,16 @@ import scala.collection.mutable
 class Set_UT {
 
   @Test
-  def test(): Unit = {
+  def add(): Unit = {
 
+    // when
     val fruit = mutable.Set("apple", "orange", "peach", "banana")
 
+    // when
     Assert.assertFalse(fruit.add("peach"))
     Assert.assertTrue(fruit.add("kiwi"))
 
-
+    // then
     Assert.assertEquals("banana,apple,orange,kiwi,peach", fruit.mkString(","))
   }
 
