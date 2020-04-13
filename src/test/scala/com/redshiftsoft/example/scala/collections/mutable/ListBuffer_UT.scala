dffd3383f20d7e3525f9ca47c7034b9d66634214
Keith David Winkler <kdwink@monsanto.com>
2020-04-13 14:16:16 -0500

    improve

--- src/test/scala/com/redshiftsoft/example/scala/collections/mutable/ListBuffer_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/mutable/ListBuffer_UT.scala
@@ -6,13 +6,17 @@ class ListBuffer_UT {
 
   @Test def insert(): Unit = {
     val list = collection.mutable.ListBuffer[String]("aa", "bb", "cc")
+    // when
     list.insert(0, "00")
+    // then
     Assert.assertEquals("ListBuffer(00, aa, bb, cc)", list.toString())
   }
 
   @Test def append(): Unit = {
     val list = collection.mutable.ListBuffer[String]("aa", "bb", "cc")
+    // when
     list.append("dd")
+    // then
     Assert.assertEquals("ListBuffer(aa, bb, cc, dd)", list.toString())
   }
 
