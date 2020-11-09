2069db789357ea83a25bea94bb7f334efcb74de8
Keith David Winkler <keith.winkler@bayer.com>
2020-11-09 09:19:22 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/collections/immutable/HashSet_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/immutable/HashSet_UT.scala
@@ -14,5 +14,26 @@ class HashSet_UT {
     Assert.assertFalse(set.contains(2000))
   }
 
+  @Test def constructionFromSeq(): Unit = {
+    // given
+    val seq = Seq(1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000)
+
+    // when
+    val set1 = HashSet() ++ seq
+    val set2 = HashSet(seq.toArray: _*)
+
+    // then
+    Assert.assertTrue(set1.contains(1000))
+    Assert.assertFalse(set1.contains(2000))
+    Assert.assertTrue(set2.contains(1000))
+    Assert.assertFalse(set2.contains(2000))
+
+    Assert.assertTrue(set1.isInstanceOf[HashSet[Int]])
+    Assert.assertTrue(set2.isInstanceOf[HashSet[Int]])
+    Assert.assertEquals("HashTrieSet", set1.getClass.getSimpleName)
+    Assert.assertEquals("HashTrieSet", set2.getClass.getSimpleName)
+
+  }
+
 
 }
