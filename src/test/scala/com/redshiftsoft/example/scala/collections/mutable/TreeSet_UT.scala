d24dd65c42aae9065aa257bf30393f4813e980c9
Keith David Winkler <keith.winkler@bayer.com>
2020-11-09 09:07:47 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/collections/mutable/TreeSet_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/mutable/TreeSet_UT.scala
@@ -1,12 +1,19 @@
-package com.redshiftsoft.example.scala.collections
+package com.redshiftsoft.example.scala.collections.mutable
 
-import org.junit.Test
+import org.junit.{Assert, Test}
 
 import scala.collection.mutable.TreeSet
 import scala.util.Random
 
 
 class TreeSet_UT {
+
+  @Test def construction(): Unit = {
+    val set = TreeSet(1, 10, 100, 1000, 10000)
+
+    Assert.assertTrue(set.contains(1000))
+    Assert.assertFalse(set.contains(2000))
+  }
 
   @Test def speed(): Unit = {
 
