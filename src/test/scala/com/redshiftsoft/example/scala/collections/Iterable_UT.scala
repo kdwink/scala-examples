0004c6f420ab17d4de4bf4a5b63177632b249a06
Keith David Winkler <keithdwinkler@gmail.com>
2021-01-30 23:55:51 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/collections/Iterable_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/Iterable_UT.scala
@@ -21,14 +21,20 @@ class Iterable_UT {
 
   @Test def assignment(): Unit = {
 
+    // iterable in mutable can only be assigned mutable
     val i1: mutable.Iterable[String] = ArrayBuffer("a", "b", "c")
+    // iterable in immutable can only be assigned immutable
     val i2: immutable.Iterable[String] = Vector("a", "b", "c")
+
+    // iterable in collection can be assigned either
+    val i3: scala.collection.Iterable[String] = ArrayBuffer("a", "b", "c")
+    val i4: scala.collection.Iterable[String] = Vector("a", "b", "c")
 
     /**
      * Both of the following conversions iterate over the elements and
      * create new data structures.
      */
-    val i4: immutable.Iterable[String] = ArrayBuffer("a", "b", "c").toIndexedSeq
-    val i5: immutable.Iterable[String] = ArrayBuffer("a", "b", "c").toSeq
+    val i5: immutable.Iterable[String] = ArrayBuffer("a", "b", "c").toIndexedSeq
+    val i6: immutable.Iterable[String] = ArrayBuffer("a", "b", "c").toSeq
   }
 }
