6ec7c5b7158235867cf343ee7f394e35d722dbcc
Keith David Winkler <keithdwinkler@gmail.com>
2021-01-30 20:39:59 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/par/Parallel_Seq_Iteration_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/par/Parallel_Seq_Iteration_UT.scala
@@ -1,11 +1,10 @@
 package com.redshiftsoft.example.scala.par
 
 import java.util.concurrent.Executors
-
 import org.junit.Test
 
 import scala.collection.parallel.ForkJoinTaskSupport
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
 
 class Parallel_Seq_Iteration_UT {
 
@@ -19,7 +18,7 @@ class Parallel_Seq_Iteration_UT {
     val array = Array(1, 2, 3, 4, 5, 6, 7, 8, 10)
 
     // This has no affect.
-    implicit val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(1))
+    implicit val ec: ExecutionContextExecutor = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(1))
 
     array.par.foreach(i => {
       println(s"[${Thread.currentThread().getName}] got number: " + i)
@@ -31,7 +30,7 @@ class Parallel_Seq_Iteration_UT {
 
     val array = Array(1, 2, 3, 4, 5, 6, 7, 8, 10).par
 
-    array.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(2))
+    array.tasksupport = new ForkJoinTaskSupport(new java.util.concurrent.ForkJoinPool(2))
 
     array.foreach(i => {
       println(s"[${Thread.currentThread().getName}] got number: " + i)
