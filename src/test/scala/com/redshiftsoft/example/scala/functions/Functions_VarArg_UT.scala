a81a17ac19b8a4ed94c670bcf7f0ec66f8b526c0
Keith David Winkler <keithdwinkler@gmail.com>
2021-02-06 20:21:49 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/functions/Functions_VarArg_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/functions/Functions_VarArg_UT.scala
@@ -15,4 +15,31 @@ class Functions_VarArg_UT {
     Assert.assertEquals("", accessDBFunc())
   }
 
+  @Test def varArgIsImmutableSeq(): Unit = {
+    def func(ints: Int*): Unit = {
+      Assert.assertTrue(ints.isInstanceOf[scala.collection.immutable.Seq[Int]])
+    }
+
+    func(1, 2, 3)
+  }
+
+  @Test def passingArraysAndSeqs(): Unit = {
+    def func(ints: Int*): Unit = {
+      Assert.assertTrue(ints.isInstanceOf[scala.collection.immutable.Seq[Int]])
+      Assert.assertEquals(ints.length, 6)
+    }
+
+    val array: Array[Int] = Array(1, 2, 3, 4, 5, 6)
+    func(array: _*)
+
+    val immutableSeq: scala.collection.immutable.Seq[Int] =
+      scala.collection.immutable.Seq[Int](1, 2, 3, 4, 5, 6)
+    func(immutableSeq: _*)
+
+    val mutableSeq: scala.collection.mutable.Seq[Int] =
+      scala.collection.mutable.Seq[Int](1, 2, 3, 4, 5, 6)
+    func(mutableSeq.toSeq: _*)
+
+  }
+
 }
