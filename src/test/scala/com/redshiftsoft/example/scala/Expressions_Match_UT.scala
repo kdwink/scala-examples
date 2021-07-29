f6b6977594ace54154a213e78a608f7a06068bf7
Keith David Winkler <keith.winkler@bayer.com>
2021-07-29 09:47:59 -0500

    dependency updates

--- src/test/scala/com/redshiftsoft/example/scala/Expressions_Match_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Expressions_Match_UT.scala
@@ -7,10 +7,11 @@ import org.junit.Test
   * Pattern Matching: https://docs.scala-lang.org/tour/pattern-matching.html
   *
   * There is no "fall through" in the matching cases, one and only one case statement is executed.
+  * If multiple case conditions match, the first is executed.
   */
 class Expressions_Match_UT {
 
-  @Test def matchExpression(): Unit = {
+  @Test def match_simple(): Unit = {
     val x = 100
     val message = x match {
       case 2 | 3 | 7 =>
@@ -27,7 +28,20 @@ class Expressions_Match_UT {
     assertEquals("error", message)
   }
 
-  @Test def matchExpressionOther(): Unit = {
+  @Test def match_with_guard(): Unit = {
+    val x = Option(9)
+    val message = x match {
+      case Some(x) if x < 10 =>
+        "case 1"
+      case Some(y) if y < 100 =>
+        "case 2"
+      case others =>
+        "case 3"
+    }
+    assertEquals("case 1", message)
+  }
+
+  @Test def match_other(): Unit = {
     val x = 100
     val message = x match {
       case 2 | 3 | 7 =>
@@ -38,7 +52,7 @@ class Expressions_Match_UT {
     assertEquals("could not find case", message)
   }
 
-  @Test def matchExpressionOtherWithBindVariable(): Unit = {
+  @Test def matchE_OtherWithBindVariable(): Unit = {
     val x = 100
     val message = x match {
       case 2 | 3 | 7 =>
