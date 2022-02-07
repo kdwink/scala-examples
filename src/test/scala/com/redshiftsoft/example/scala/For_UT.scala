2b31cac19197f4712e2c605c392065ac3c4274bc
Keith David Winkler <keith.winkler@bayer.com>
2022-02-06 22:27:08 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/For_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/For_UT.scala
@@ -2,6 +2,8 @@ package com.redshiftsoft.example.scala
 
 import org.junit.{Assert, Test}
 
+import scala.collection.mutable.ListBuffer
+
 class For_UT {
 
   @Test def simple(): Unit = {
@@ -12,7 +14,6 @@ class For_UT {
     Assert.assertEquals(55, count)
   }
 
-
   @Test def forOneToOne(): Unit = {
     var count = 0
     for (x <- 1 to 1) {
@@ -20,5 +21,15 @@ class For_UT {
     }
     Assert.assertEquals(1, count)
   }
+
+  @Test def sequence(): Unit = {
+    var seq = Seq("a", "bb", "ccc", "dddd", "eeeee")
+    val buffer = ListBuffer.empty[String]
+    for (x <- seq.reverse) {
+      buffer.append(x)
+    }
+    Assert.assertEquals(Seq("eeeee", "dddd", "ccc", "bb", "a"), buffer.toSeq)
+  }
+
 
 }
