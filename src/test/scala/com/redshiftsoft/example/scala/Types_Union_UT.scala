a9d8750852e17c5494a60a9adf42bcff377d3816
Keith David Winkler <keith.winkler@bayer.com>
2022-03-29 10:30:13 -0500

    improve

---
+++ src/test/scala/com/redshiftsoft/example/scala/Types_Union_UT.scala
@@ -0,0 +1,13 @@
+package com.redshiftsoft.example.scala
+
+import com.redshiftsoft.example.scalatest.BaseSpec
+
+/**
+  * https://docs.scala-lang.org/scala3/reference/new-types/union-types.html
+  */
+class Types_Union_UT extends BaseSpec {
+
+  "bla" should "bla" in {
+  }
+
+}
---
+++ src/test/scala/com/redshiftsoft/example/scalatest/BaseSpec.scala
@@ -0,0 +1,8 @@
+package com.redshiftsoft.example.scalatest
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class BaseSpec extends AnyFlatSpec with should.Matchers {
+
+}
