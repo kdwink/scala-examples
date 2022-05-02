cebce49ab6971fde672da670f3ee7daae2d9992e
Keith David Winkler <keithdwinkler@gmail.com>
2022-05-01 22:25:57 -0500

    improve

--- src/test/scala/com/redshiftsoft/example/code/graph/GraphAppSpec.scala
+++ src/test/scala/com/redshiftsoft/example/code/graph/GraphAppSpec.scala
@@ -14,16 +14,17 @@ class GraphAppSpec extends BaseSpec {
    *      --> B
    *            --> B_1
    *               --> B_1_1
+   *               --> B_1_2
    *            --> B_2
    *               --> B_2_1
    */
   "count" should "count correctly" in {
     val root: N = N("root", Seq(
       N("A", Seq(N("A_1", Seq()), N("A_2", Seq(N("A_2_1"), N("A_2_2"), N("A_2_3"))))),
-      N("B", Seq(N("B_1", Seq(N("B_1_1"))), N("B_2", Seq(N("B_2_1"))))))
+      N("B", Seq(N("B_1", Seq(N("B_1_1"), N("B_1_2"))), N("B_2", Seq(N("B_2_1"))))))
     )
 
-    count(root) should be(12)
+    count(root) should be(13)
   }
 
   def count(n: N): Int = {
