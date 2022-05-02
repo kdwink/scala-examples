86161e2f174c4a3ee6b904080edafb3d114195ab
Keith David Winkler <keithdwinkler@gmail.com>
2022-05-01 22:36:46 -0500

    improve

--- src/test/scala/com/redshiftsoft/example/scala/collections/mutable/Queue_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/mutable/Queue_UT.scala
@@ -8,12 +8,14 @@ class Queue_UT extends BaseSpec {
 
   "dequeue" should "be fifo" in {
 
+    // given
     val q = mutable.Queue[String]()
 
     q.enqueue("aa")
     q.enqueue("bb")
     q.enqueue("cc")
 
+    // when / then
     q.dequeue() should be("aa")
     q.dequeue() should be("bb")
     q.dequeue() should be("cc")
