c25302e82df3b77ed63d88c3edddccca5a9810f7
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-09 12:54:53 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/Types_Union_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Types_Union_UT.scala
@@ -7,7 +7,26 @@ import com.redshiftsoft.example.scalatest.BaseSpec
  */
 class Types_Union_UT extends BaseSpec {
 
-  "bla" should "bla" in {
+  class Hash(val name: String)
+
+  trait ID
+
+  case class UserName(name: String) extends ID
+
+  case class Password(hash: Hash) extends ID
+
+  def help(id: UserName | Password): String =
+    val user: String = id match
+      case UserName(name) => name
+      case Password(hash) => hash.name
+    user
+
+  "either class" should "be a valid argument to help" in {
+    val u1 = UserName("username_username")
+    val p1 = Password(Hash("password_username"))
+
+    help(u1) should be("username_username")
+    help(p1) should be("password_username")
   }
 
 }
