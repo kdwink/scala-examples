301fa1fde6efe1cecd9fc463e9175e4471fcff92
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-09 12:47:37 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/Types_Intersect_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Types_Intersect_UT.scala
@@ -2,13 +2,34 @@ package com.redshiftsoft.example.scala
 
 import com.redshiftsoft.example.scalatest.BaseSpec
 
+trait Resettable:
+  def reset(): Unit
+
+trait Growable[T]:
+  def add(t: T): Unit
+
+class X[T] extends Resettable, Growable[T] {
+  override def reset(): Unit = None
+
+  override def add(t: T): Unit = None
+}
+
+def function(x: Resettable & Growable[String]): Unit =
+  x.reset()
+  x.add("first")
+
 /**
  * https://docs.scala-lang.org/scala3/reference/new-types/intersection-types.html
  */
 class Types_Intersect_UT extends BaseSpec {
 
-  "bla" should "bla" in {
 
+  "bla" should "bla" in {
+    // then
+    val o1 = new X[String]
+
+    // then
+    function(o1)
   }
 
 }
