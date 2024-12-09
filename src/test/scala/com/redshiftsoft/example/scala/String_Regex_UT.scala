2cd81b0096736dc1650ab1d35753126bafd81a03
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-09 12:25:56 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/String_Regex_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/String_Regex_UT.scala
@@ -1,17 +1,23 @@
 package com.redshiftsoft.example.scala
 
-import org.junit.jupiter.api.Assertions.{assertEquals, assertFalse, assertTrue}
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+
+import scala.util.matching.Regex
 
 
 class String_Regex_UT {
 
   @Test def findFirstIn(): Unit = {
-    val startsWithARegEx = "A.{3}".r
+    // given
+    val startsWithARegEx: Regex = "A.{3}".r
 
+    // when
     val matchOption = startsWithARegEx.findFirstIn("bla bla A123456789")
 
+    // then
     assertEquals("A123", matchOption.get)
+    assertEquals("scala.util.matching.Regex", startsWithARegEx.getClass.getName)
   }
 
 
