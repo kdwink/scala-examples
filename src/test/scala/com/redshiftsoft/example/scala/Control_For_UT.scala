7625c51c4cff122fb843ba6dbaf792059fdd5565
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-13 13:05:21 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/Control_For_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Control_For_UT.scala
@@ -8,27 +8,27 @@ import scala.collection.mutable.ListBuffer
 
 class Control_For_UT:
 
-  @Test def simple(): Unit = 
+  @Test def simple(): Unit =
     var count = 0
     for (x <- 1 to 10) {
       count = count + x
     }
     assertEquals(55, count)
 
-  @Test def guard(): Unit = 
+  @Test def guard(): Unit =
     val result = ListBuffer.empty[Int]
     for (x <- 1 to 10 if x % 3 == 0)
       result.append(x)
     assertEquals(Seq(3, 6, 9), result.toSeq)
 
-  @Test def forOneToOne(): Unit = 
+  @Test def forOneToOne(): Unit =
     var count = 0
     for (x <- 1 to 1) {
       count = count + x
     }
     assertEquals(1, count)
 
-  @Test def sequence(): Unit = 
+  @Test def sequence(): Unit =
     val seq = Seq("a", "bb", "ccc", "dddd", "eeeee")
     val buffer = ListBuffer.empty[String]
     for (x <- seq.reverse) {
@@ -36,7 +36,7 @@ class Control_For_UT:
     }
     assertEquals(Seq("eeeee", "dddd", "ccc", "bb", "a"), buffer.toSeq)
 
-  @Test def sequenceIndex(): Unit = 
+  @Test def sequenceIndex(): Unit =
     val seq = Seq("a", "bb", "ccc", "dddd", "eeeee")
     val buffer = ListBuffer.empty[String]
     for (i <- seq.indices) {
