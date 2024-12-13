fbf7cfd637f58481f9768b8de68ca36a5502a2dc
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-13 12:58:30 -0600

    improve

---
+++ src/test/scala/com/redshiftsoft/example/scala/Class_Open_UT.scala
@@ -0,0 +1,19 @@
+package com.redshiftsoft.example.scala
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+/**
+ * Marking classes with open is a new feature of Scala 3. Having to explicitly mark classes as open avoids many common 
+ * pitfalls in OO design. In particular, it requires library designers to explicitly plan for extension and for
+ * instance document the classes that are marked as open with additional extension contracts.
+ */
+class Class_Open_UT:
+
+  open class Person(val name: String)
+
+  @Test
+  def test(): Unit = {
+    val p = Person("george")
+    assertEquals("george", p.name)
+  }
