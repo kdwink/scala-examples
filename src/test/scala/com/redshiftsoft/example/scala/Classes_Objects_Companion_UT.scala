fb203692700feb492f88824a764d8ac65c2362b3
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-13 12:34:58 -0600

    improve

---
+++ src/test/scala/com/redshiftsoft/example/scala/Classes_Objects_Companion_UT.scala
@@ -0,0 +1,39 @@
+package com.redshiftsoft.example.scala
+
+import org.junit.jupiter.api.Assertions.{assertEquals, assertFalse, assertTrue}
+import org.junit.jupiter.api.Test
+
+/**
+ * https://docs.scala-lang.org/scala3/book/taste-objects.html
+ */
+class Classes_Objects_Companion_UT {
+
+  /* Companion object can access private fields of class. */
+  @Test
+  def objectApplyAsFactoryPattern(): Unit = {
+
+    class MyObject {
+      val WHATEVER = 100
+      private var age: Int = 10
+      private var name: String = "george"
+
+      def getName: String = {
+        name
+      }
+    }
+    object MyObject {
+
+      def apply(s: String): MyObject = {
+        val m = new MyObject
+        m.name = s
+        m.age = 100
+        m
+      }
+    }
+
+    val s = MyObject("Keith")
+
+    assertEquals("Keith", s.getName)
+  }
+
+}
--- src/test/scala/com/redshiftsoft/example/scala/Classes_Objects_Singleton_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Classes_Objects_Singleton_UT.scala
@@ -22,32 +22,4 @@ class Classes_Objects_Singleton_UT {
 
   }
 
-  /* Companion object can access private fields of class. */
-  @Test
-  def objectApplyAsFactoryPattern(): Unit = {
-
-    class MyObject {
-      val WHATEVER = 100
-      private var age: Int = 10
-      private var name: String = "george"
-
-      def getName: String = {
-        name
-      }
-    }
-    object MyObject {
-
-      def apply(s: String): MyObject = {
-        val m = new MyObject
-        m.name = s
-        m.age = 100
-        m
-      }
-    }
-
-    val s = MyObject("Keith")
-
-    assertEquals("Keith", s.getName)
-  }
-
 }
