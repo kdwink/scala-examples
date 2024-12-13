985cc7ef76ef8e4ff61145341612b70aa5ffa156
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-13 13:52:29 -0600

    improve

---
+++ src/test/scala/com/redshiftsoft/example/scala/classes/Class_Inheritance_Extension_UT.scala
@@ -0,0 +1,30 @@
+package com.redshiftsoft.example.scala.classes
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Class_Inheritance_Extension_UT:
+
+
+  /**
+   * https://docs.scala-lang.org/scala3/book/ca-extension-methods.html
+   */
+  @Test def inheritance_extension(): Unit =
+    class A(var field: Long):
+      def method1 = "hello 1"
+
+      def method2 = "hello 2"
+
+    extension (a: A)
+      def method3: Long = 42L
+      def method4: Long = 142L
+      def incrementField(): Unit = {
+        a.field = a.field + 1_000
+      }
+
+    val a = new A(300)
+    a.incrementField()
+    assertEquals("hello 1", a.method1)
+    assertEquals(42, a.method3)
+    assertEquals(142, a.method4)
+    assertEquals(1_300, a.field)
--- src/test/scala/com/redshiftsoft/example/scala/classes/Class_Inheritance_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/classes/Class_Inheritance_UT.scala
@@ -20,30 +20,6 @@ class Class_Inheritance_UT:
     assertEquals("hello 1", c.method1)
     assertEquals("hello B", c.method2)
 
-  /**
-   * https://docs.scala-lang.org/scala3/book/ca-extension-methods.html
-   */
-  @Test def inheritance_extension(): Unit =
-    class A(var field: Long):
-      def method1 = "hello 1"
-
-      def method2 = "hello 2"
-
-    extension (a: A)
-      def method3: Long = 42L
-      def method4: Long = 142L
-      def incrementField(): Unit = {
-        a.field = a.field + 1_000
-      }
-
-    val a = new A(300)
-    a.incrementField()
-    assertEquals("hello 1", a.method1)
-    assertEquals(42, a.method3)
-    assertEquals(142, a.method4)
-    assertEquals(1_300, a.field)
-
-
   @Test def polymorphism(): Unit =
     class A {
       def method1 = "hello 1"
