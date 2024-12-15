fe686b6e60ef83bc13a98ffb405f9c46235f1661
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-15 17:11:24 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/functions/Functions_Partial_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/functions/Functions_Partial_UT.scala
@@ -5,25 +5,22 @@ import org.junit.jupiter.api.Test
 
 import scala.util.Try
 
-class Functions_Partial_UT {
+class Functions_Partial_UT:
 
-  private val statusHandler1: PartialFunction[Int, String] = {
+  private val statusHandler1: PartialFunction[Int, String] =
     case 100 => "Huh"
-  }
 
-  private val statusHandler2: PartialFunction[Int, String] = {
+  private val statusHandler2: PartialFunction[Int, String] =
     case 200 => "Okay"
     case 400 => "Error"
     case 500 => "Our Error"
-  }
 
-  @Test def invokeDirectly(): Unit = {
+  @Test def invokeDirectly(): Unit =
     assertEquals("Okay", statusHandler2(200))
     assertEquals("Error", statusHandler2(400))
     assertEquals("Our Error", statusHandler2(500))
-  }
 
-  @Test def partialMatchError(): Unit = {
+  @Test def partialMatchError(): Unit =
     var flag: Boolean = false
     try {
       assertEquals("Our Error", statusHandler2(550))
@@ -32,22 +29,19 @@ class Functions_Partial_UT {
         flag = true
     }
     assertTrue(flag)
-  }
 
   //noinspection AccessorLikeMethodIsUnit
-  @Test def isDefinedAt(): Unit = {
+  @Test def isDefinedAt(): Unit =
     assertTrue(statusHandler1.isDefinedAt(100))
     assertFalse(statusHandler1.isDefinedAt(200))
     assertTrue(statusHandler2.isDefinedAt(200))
-  }
 
-  @Test def orElse(): Unit = {
+  @Test def orElse(): Unit =
     assertEquals("Huh", statusHandler1.orElse(statusHandler2)(100))
     assertEquals("Our Error", statusHandler1.orElse(statusHandler2)(500))
-  }
 
   /** collect will filter stream to only elements on which partial function is defined. */
-  @Test def useWithCollect(): Unit = {
+  @Test def useWithCollect(): Unit =
     // given
     def stringify: PartialFunction[Int, String] = {
       case 10 => "ten"
@@ -56,10 +50,9 @@ class Functions_Partial_UT {
     }
     // when/then
     assertEquals("List(ten, twenty, hundred)", Seq(10, 20, 30, 40, 50, 60, 100).collect(stringify).toString)
-  }
 
   /** collect will filter stream to only elements on which partial function is defined. */
-  @Test def useWithMap(): Unit = {
+  @Test def useWithMap(): Unit =
     // given
     def stringify: PartialFunction[Int, String] = {
       case 10 => "ten"
@@ -72,10 +65,9 @@ class Functions_Partial_UT {
     // then
     assertFalse(theTry.isSuccess)
     assertTrue(theTry.failed.get.isInstanceOf[MatchError])
-  }
 
 
-  @Test def useWithTry(): Unit = {
+  @Test def useWithTry(): Unit =
     def handleError: PartialFunction[Throwable, Int] = {
       case t: IllegalStateException =>
         111
@@ -92,7 +84,3 @@ class Functions_Partial_UT {
     val recoveredTry = theTry.recover(handleError)
     assertTrue(recoveredTry.isSuccess)
     assertEquals(111, recoveredTry.get)
-  }
-
-
-}
