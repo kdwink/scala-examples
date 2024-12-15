9ceb626086a4b76897a097855473467e2965ae2d
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-15 17:05:22 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/String_Interpolation_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/String_Interpolation_UT.scala
@@ -12,6 +12,10 @@ class String_Interpolation_UT {
     assertEquals("2pi is 6.28318", s"2pi is ${pi * 2}")
   }
 
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // formatted
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
   @Test def interpolation_formatting_floating_point(): Unit = {
     assertEquals("value is 204,686,706", f"value is ${102343353 * 2}%,d")
     assertEquals("value is 12,792,919.13", f"value is ${102343353.0 / 8}%,.2f")
@@ -29,10 +33,33 @@ class String_Interpolation_UT {
     assertEquals("value: 16.25    ", f"value: ${d * 2}%-9s")
   }
 
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // raw
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
   @Test def raw(): Unit = {
     val test = raw"a \n b"
 
     assertEquals("a \\n b", test)
   }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // custom
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  @Test def custom(): Unit =
+    // given
+    case class Point(x: Double, y: Double)
+
+    extension (sc: StringContext)
+      private def p(args: Any*): Point =
+        val a = sc.parts.head.split(",")
+        Point(a(0).toDouble, a(1).toDouble)
+
+    // when
+    val pt = p"1,-2"
+
+    // then
+    assertEquals(Point(1.0, -2.0), pt)
 
 }
