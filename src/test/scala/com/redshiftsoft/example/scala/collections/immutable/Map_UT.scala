aaf37bfb3162efe776f41c655da0b1ed33aeb9f4
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-15 21:50:21 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/collections/immutable/Map_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/immutable/Map_UT.scala
@@ -4,69 +4,56 @@ import org.junit.jupiter.api.Assertions.{assertEquals, assertFalse, assertThrows
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.function.Executable
 
-class Map_UT {
+class Map_UT:
 
   private val colorMap = Map("red" -> 0xFF0000, "green" -> 0xF00, "blue" -> 0xFF)
 
-  @Test def declaring(): Unit = {
+  @Test def declaring(): Unit =
     assertEquals(3, colorMap.size)
-  }
 
-  @Test def accessing(): Unit = {
+  @Test def accessing(): Unit =
     assertEquals(0xFF0000, colorMap("red"))
-  }
 
   @Test
-  def accessing_NonExistingKey(): Unit = {
+  def accessing_NonExistingKey(): Unit =
     assertThrows(classOf[java.util.NoSuchElementException], () => colorMap("frank"))
-  }
 
   //noinspection MapGetGet
-  @Test def accessing_WithGet(): Unit = {
+  @Test def accessing_WithGet(): Unit =
     assertEquals(0xFF0000, colorMap.get("red").get)
-  }
 
   @Test
-  def accessing_WithGet_NonExistingKey(): Unit = {
+  def accessing_WithGet_NonExistingKey(): Unit =
     assertThrows(classOf[java.util.NoSuchElementException], () => {
       val maybeInt: Option[Int] = colorMap.get("frank")
       assertEquals(0xFF0000, maybeInt.get)
     })
-  }
 
-  @Test def contains(): Unit = {
+  @Test def contains(): Unit =
     assertTrue(colorMap.contains("red"))
     assertFalse(colorMap.contains("yellow"))
-  }
 
-  @Test def iterating(): Unit = {
+  @Test def iterating(): Unit =
     var sum = 0
     for (pair <- colorMap) {
       sum = sum + pair._2
     }
 
     assertEquals(16715775, sum)
-  }
 
-  @Test def testToSeq(): Unit = {
+  @Test def testToSeq(): Unit =
     val resultList: Seq[(String, Int)] = colorMap.toSeq
     assertEquals(3, resultList.size)
     assertTrue(resultList(1).isInstanceOf[Tuple2[String, Int]])
-  }
 
-  @Test def mapToTuples(): Unit = {
+  @Test def mapToTuples(): Unit =
     val tuplesIterator: Iterable[(String, Int)] = colorMap.keys.map(k => (k, colorMap(k)))
     var count = 0
     for (x <- tuplesIterator) {
       count = count + 1
     }
     assertEquals(3, count)
-  }
 
-  @Test def values(): Unit = {
+  @Test def values(): Unit =
     val values = colorMap.values.toSeq
     assertEquals(Seq(0xFF0000, 0xF00, 0xFF), values)
-  }
-
-
-}
--- src/test/scala/com/redshiftsoft/example/scala/collections/mutable/HashMap_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/mutable/HashMap_UT.scala
@@ -6,34 +6,30 @@ import org.junit.jupiter.api.Test
 
 import scala.collection.mutable
 
-class HashMap_UT {
+class HashMap_UT:
 
-  @Test def put(): Unit = {
+  @Test def put(): Unit =
     val map: mutable.Map[String, String] = mutable.HashMap.empty
     map.put("key1", "value1")
     assertEquals(1, map.size)
-  }
 
-  @Test def passMutableMap(): Unit = {
+  @Test def passMutableMap(): Unit =
     // given
     val map: mutable.Map[String, Double] = mutable.HashMap.empty
     // when/then
     iTakeMutableMap(map)
     iTakeImmutableMap1(map.toMap)
     iTakeImmutableMap2(map.toMap)
-  }
 
-  @Test def from(): Unit = {
+  @Test def from(): Unit =
     val immutable = Map(1 -> 'a', 2 -> 'b', 3 -> 'c', 4 -> 'd', 5 -> 'e', 6 -> 'f')
     val hashMap = scala.collection.mutable.Map.from(immutable)
 
     assertEquals("scala.collection.mutable.HashMap", hashMap.getClass.getName)
     assertEquals(6, hashMap.size)
-  }
 
 
   private def iTakeMutableMap(m: mutable.Map[String, Double]): Unit = {
-
   }
 
   private def iTakeImmutableMap1(m: collection.immutable.Map[String, Double]): Unit = {
@@ -43,5 +39,3 @@ class HashMap_UT {
   private def iTakeImmutableMap2(m: Map[String, Double]): Unit = {
 
   }
-
-}
