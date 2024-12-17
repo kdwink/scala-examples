f8c68cff63447a8b90ac44e484c61c99d5d31dc6
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-17 08:24:49 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/collections/IterableOnce_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/IterableOnce_UT.scala
@@ -19,3 +19,41 @@ class IterableOnce_UT:
     val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
     val result = numbers.reduce(1 + _ + _)
     assertEquals(53, result)
+
+  //noinspection AccessorLikeMethodIsUnit
+  @Test def toMap_1(): Unit =
+    val v: Vector[(String, Long)] = Vector(
+      ("aaa", 1),
+      ("bbb", 10),
+      ("aaa", 2),
+      ("bbb", 20),
+      ("aaa", 3),
+      ("bbb", 30),
+    )
+
+    // when
+    val m: Map[String, Long] = v.toMap
+
+    // then
+    assertEquals(2, m.size)
+    assertEquals(3, m("aaa"))
+    assertEquals(30, m("bbb"))
+
+
+  //noinspection AccessorLikeMethodIsUnit
+  @Test def toMap_2(): Unit =
+
+    val v: Vector[(String, Seq[Long])] = Vector(
+      ("aaa", Seq(1, 2, 3)),
+      ("bbb", Seq(10, 11, 12)),
+      ("aaa", Seq(4, 5, 6)),
+      ("bbb", Seq(13, 14, 15))
+    )
+
+    // when
+    val m: Map[String, Seq[Long]] = v.toMap
+
+    // then
+    assertEquals(2, m.size)
+    assertEquals(Seq(4, 5, 6), m("aaa"))
+    assertEquals(Seq(13, 14, 15), m("bbb"))
--- src/test/scala/com/redshiftsoft/example/scala/collections/immutable/seq/Vector_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/immutable/seq/Vector_UT.scala
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.Test
 /**
  * Vector, an immutable Seq, which, unlike List, provides constant time implementations of all methods.
  */
-class Vector_UT {
+class Vector_UT
 
-  @Test def construction(): Unit = {
+  @Test def construction(): Unit =
 
     val v1 = Vector(1, 2, 3)
     val v2 = Vector(10, 20, 30)
@@ -18,50 +18,8 @@ class Vector_UT {
 
     assertEquals("scala.collection.immutable.Vector1", v3.getClass.getName)
     assertEquals("Vector(1, 2, 3, 10, 20, 30)", v3.toString())
-  }
 
-  //noinspection AccessorLikeMethodIsUnit
-  @Test def toMap_1(): Unit = {
-
-    val v: Vector[(String, Long)] = Vector(
-      ("aaa", 1),
-      ("bbb", 10),
-      ("aaa", 2),
-      ("bbb", 20),
-      ("aaa", 3),
-      ("bbb", 30),
-    )
-
-    // when
-    val m: Map[String, Long] = v.toMap
-
-    // then
-    assertEquals(2, m.size)
-    assertEquals(3, m("aaa"))
-    assertEquals(30, m("bbb"))
-  }
-
-
-  //noinspection AccessorLikeMethodIsUnit
-  @Test def toMap_2(): Unit = {
-
-    val v: Vector[(String, Seq[Long])] = Vector(
-      ("aaa", Seq(1, 2, 3)),
-      ("bbb", Seq(10, 11, 12)),
-      ("aaa", Seq(4, 5, 6)),
-      ("bbb", Seq(13, 14, 15))
-    )
-
-    // when
-    val m: Map[String, Seq[Long]] = v.toMap
-
-    // then
-    assertEquals(2, m.size)
-    assertEquals(Seq(4, 5, 6), m("aaa"))
-    assertEquals(Seq(13, 14, 15), m("bbb"))
-  }
-
-  @Test def groupMap(): Unit = {
+  @Test def groupMap(): Unit =
 
     val v: Vector[(String, Long)] = Vector(
       ("aaa", 1),
@@ -79,9 +37,8 @@ class Vector_UT {
     assertEquals(2, m.size)
     assertEquals(Seq(1, 2, 3), m("aaa"))
     assertEquals(Seq(10, 20, 30), m("bbb"))
-  }
 
-  @Test def groupMap_combineValues(): Unit = {
+  @Test def groupMap_combineValues(): Unit =
 
     val v: Vector[(String, Seq[Long])] = Vector(
       ("aaa", Seq(1, 2, 3)),
@@ -100,6 +57,3 @@ class Vector_UT {
     assertEquals(2, m2.size)
     assertEquals(Seq(1, 2, 3, 100, 200, 300, -10, -20, -30), m2("aaa"))
     assertEquals(Seq(10, 20, 30, 1000, 2000, 3000, -100, -200, -300), m2("bbb"))
-  }
-
-}
