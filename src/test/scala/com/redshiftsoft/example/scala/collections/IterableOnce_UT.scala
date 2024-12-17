c6a5c15400fb408812e0044183ceb44246203efc
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-17 08:20:15 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/collections/IterableOnce_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/collections/IterableOnce_UT.scala
@@ -10,12 +10,12 @@ class IterableOnce_UT:
     val result = numbers.sum
     assertEquals(15, result)
 
-@Test def reduce(): Unit =
-  val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
-  val result = numbers.reduce((a, b) => 1 + a + b)
-  assertEquals(53, result)
+  @Test def reduce(): Unit =
+    val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val result = numbers.reduce((a, b) => 1 + a + b)
+    assertEquals(53, result)
 
-@Test def reduce_withPlaceholders(): Unit =
-  val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
-  val result = numbers.reduce(1 + _ + _)
-  assertEquals(53, result)
+  @Test def reduce_withPlaceholders(): Unit =
+    val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val result = numbers.reduce(1 + _ + _)
+    assertEquals(53, result)
