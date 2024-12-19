6eabca6cb6f9d6ff2ce0eb0fcfee36991440c8b8
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-19 09:29:46 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/Control_For_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/Control_For_UT.scala
@@ -8,50 +8,50 @@ import scala.collection.mutable.ListBuffer
 
 class Control_For_UT:
 
-  @Test def simple(): Unit =
+  @Test def for_do_1(): Unit =
     var count = 0
     for x <- 1 to 10 do
       count = count + x
     assertEquals(55, count)
 
-  @Test def guard(): Unit =
-    val result = ListBuffer.empty[Int]
-    for x <- 1 to 10 if x % 3 == 0 do
-      result.append(x)
-    assertEquals(Seq(3, 6, 9), result.toSeq)
+  @Test def for_do_2(): Unit =
+    val elements = Seq("a", "bb", "ccc", "dddd", "eeeee", "ffffff")
+    var i = 0
+    for e <- elements do i = i + 1
+    assertEquals(6, i)
 
-  @Test def forOneToOne(): Unit =
+  @Test def for_do_1_to_1(): Unit =
     var count = 0
     for x <- 1 to 1 do
       count = count + x
     assertEquals(1, count)
 
-  @Test def sequence(): Unit =
+  @Test def for_do_guard(): Unit =
+    val result = ListBuffer.empty[Int]
+    for x <- 1 to 10 if x % 3 == 0 do
+      result.append(x)
+    assertEquals(Seq(3, 6, 9), result.toSeq)
+
+  @Test def for_do_sequence(): Unit =
     val seq = Seq("a", "bb", "ccc", "dddd", "eeeee")
     val buffer = ListBuffer.empty[String]
     for x <- seq.reverse do
       buffer.append(x)
     assertEquals(Seq("eeeee", "dddd", "ccc", "bb", "a"), buffer.toSeq)
 
-  @Test def sequenceIndex(): Unit =
+  @Test def for_do_sequence_index(): Unit =
     val seq = Seq("a", "bb", "ccc", "dddd", "eeeee")
     val buffer = ListBuffer.empty[String]
     for i <- seq.indices do
       buffer.append(seq(i))
     assertEquals(Seq("a", "bb", "ccc", "dddd", "eeeee"), buffer.toSeq)
 
-  @Test def for_do(): Unit =
-    val seq = Seq("a", "bb", "ccc", "dddd", "eeeee")
-    var i = 0
-    for s <- seq do i = i + 1
-    assertEquals(5, i)
-
-  @Test def for_do_two(): Unit =
+  @Test def for_do_multi_line(): Unit =
     val seq = Seq("a", "bb", "ccc", "dddd", "eeeee")
     var i = 0
     var j = 0
-    for s <- seq do 
-      i = i + 1 
+    for s <- seq do
+      i = i + 1
       j = j + 1
     assertEquals(5, i)
     assertEquals(5, j)
