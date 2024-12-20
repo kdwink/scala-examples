ac9c643ad00f606eb0f722f88bf12617ced8e300
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-20 12:42:27 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/par/Parallel_Seq_Iteration_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/par/Parallel_Seq_Iteration_UT.scala
@@ -17,7 +17,7 @@ class Parallel_Seq_Iteration_UT:
    *
    * https://stackoverflow.com/questions/9154691/how-to-set-the-number-of-threads-to-use-for-par
    */
-  @Test def thisDoesNotWork(): Unit =
+  @Test def par_array_does_not_use_implicit_execution_context(): Unit =
     val array = Array(1, 2, 3, 4, 5, 6, 7, 8, 10)
 
     // This has no affect.
@@ -36,12 +36,14 @@ class Parallel_Seq_Iteration_UT:
       31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
       46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60
     ).par
-    array.tasksupport = new ForkJoinTaskSupport(new java.util.concurrent.ForkJoinPool(2))
+    array.tasksupport = new ForkJoinTaskSupport(new java.util.concurrent.ForkJoinPool(3))
 
     // when
-    val distinct: ParArray[String] = array.map(i => Thread.currentThread().getName).distinct
+    val threadNames: ParArray[String] = array.map(i => Thread.currentThread().getName).distinct
 
     // then
-    assertEquals("ForkJoinPool-2-worker-1,ForkJoinPool-2-worker-2", distinct.toList.sorted.mkString(","));
+    assertEquals("worker-1,worker-2,worker-3",
+      threadNames.toList.map(s => s.replaceAll("ForkJoinPool-\\d-", "")).sorted.mkString(",")
+    );
 
 
