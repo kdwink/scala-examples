709d69f0b639030bc51ada07f37c182ae126e6af
Keith David Winkler <keithdwinkler@gmail.com>
2024-12-20 12:38:48 -0600

    improve

--- src/test/scala/com/redshiftsoft/example/scala/par/Parallel_Seq_Iteration_UT.scala
+++ src/test/scala/com/redshiftsoft/example/scala/par/Parallel_Seq_Iteration_UT.scala
@@ -29,14 +29,19 @@ class Parallel_Seq_Iteration_UT:
     })
 
   @Test def par_array_set_thread_count(): Unit =
-    val set = scala.collection.mutable.HashSet[String]()
-
-    val array: ParArray[Int] = Array(1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22).par
-
+    // given
+    val array: ParArray[Int] = Array(
+      1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15,
+      16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+      31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+      46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60
+    ).par
     array.tasksupport = new ForkJoinTaskSupport(new java.util.concurrent.ForkJoinPool(2))
 
-    array.foreach(i => set.add(Thread.currentThread().getName))
+    // when
+    val distinct: ParArray[String] = array.map(i => Thread.currentThread().getName).distinct
 
-    assertEquals("ForkJoinPool-2-worker-1,ForkJoinPool-2-worker-2", set.toSeq.sorted.mkString(","));
+    // then
+    assertEquals("ForkJoinPool-2-worker-1,ForkJoinPool-2-worker-2", distinct.toList.sorted.mkString(","));
 
 
